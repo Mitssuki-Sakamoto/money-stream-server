153ce537d336d571f5d4a0b32819092fb07ca420
mitsuki <mitsuki.sakamoto@uec.ac.jp>
2020-11-24 19:55:57 +0900

    create user and event models.

---
+++ server/app/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    public $timestamps = false;
+    //
+    protected $fillable = [
+        'name', 'invent_code', 'api_key', 'thumbnail'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'api_key',
+    ];
+
+}
